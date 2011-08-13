4ada040d19feb78e53ec3973f9b55ce483ec3222
Jinyoung Kim <jinyoungkim@gmail.com>
2011-08-13 12:33:04 -0400

    Initial Commit

---
+++ Walky/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Walky
+//
+//  Created by Jinyoung Kim on 8/13/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
